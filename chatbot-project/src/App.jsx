d80fa0dd87aa2f0bddf7f095bcdb3af23a76ae44
nitin <152731976+corenitin@users.noreply.github.com>
2026-02-01 07:48:32 +0000

    react set up

---
+++ chatbot-project/src/App.jsx
@@ -0,0 +1,191 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function ChatInput({ chatMessages, setChatMessages }) {
+
+  const [inputText, setInputText] = useState('')
+  
+
+  function saveInputText(event) {
+    setInputText(event.target.value)
+  }
+
+
+  async function sendMessage() {
+
+    // Clear the textbox at the top now because the Chatbot
+    // will take some time to load the response. We want
+    // to clear the textbox immediately rather waiting
+    // for the Chatbot to finish loading.
+    setInputText('');
+
+    const newChatMessages = [
+      ...chatMessages,
+      {
+        message: inputText,
+        sender: 'user',
+        id: crypto.randomUUID()
+      }
+    ]
+
+    setChatMessages([
+      ...newChatMessages,
+      // This creates a temporary Loading... message.
+      // Because we don't save this message in newChatMessages,
+      // it will be remove later, when we add the response.
+      {
+        message: <img src="loading-spinner.gif" className="loading-gif" />,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ]);
+
+    const response = await Chatbot.getResponseAsync(inputText);
+    console.log(Chatbot);
+
+    setChatMessages([
+      ...newChatMessages,
+      {
+        message: response,
+        sender: 'robot',
+        id: crypto.randomUUID()
+      }
+    ])
+
+    // Set isLoading to false after everything is done.
+
+  }
+
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      sendMessage();
+    } else if (event.key === 'Escape') {
+      setInputText('');
+    }
+  }
+
+  return (
+    <div className="chat-input-container">
+      <input
+        placeholder="Send a message to Chatbot"
+        size="30"
+        onChange={saveInputText}
+        onKeyDown={handleKeyDown}
+        value={inputText}
+        className="chat-input"
+      //controlled input
+      />
+      <button
+        onClick={sendMessage}
+        className="send-button"
+      >Send</button>
+    </div>
+  );
+}
+
+function ChatMessage({ message, sender }) {
+  // Destrcutring 
+  // 1. const message = props.message
+  //    const sender = props.sender
+  // 2. const {message, sender} = props
+  // 3. function ChatMessage({message, sender}) 
+
+  /*
+  if (sender === 'robot') {
+      return (
+          <div>
+              
+              {message}
+          </div>
+      )
+  }
+  */
+
+  return (
+    <div className={
+      sender === 'user'
+        ? 'chat-message-user'
+        : 'chat-message-robot'
+    }>
+      {sender === 'robot' && (
+        <img src="robot.png" className="chat-message-profile" />
+      )}
+      <div className="chat-message-text">
+        {message}
+      </div>
+      {sender === 'user' && (
+        <img src="user.png" className="chat-message-profile" />
+      )}
+    </div>
+  )
+}
+
+// To use a function as a hook, the function name must
+// start with "use".
+function useAutoScroll(dependencies) {
+  // It's highly recommend to rename this to something
+  // more generic like containerRef. This will make the
+  // code make more sense if we ever reuse this code in
+  // other components.
+  const containerRef = React.useRef(null);
+
+
+  React.useEffect(() => {
+    const containerElem = containerRef.current;
+    if (containerElem) {
+      containerElem.scrollTop = containerElem.scrollHeight;
+    }
+
+  }, dependencies);
+
+  return containerRef;
+}
+
+function ChatMessages({ chatMessages }) {
+  const chatMessagesRef = useAutoScroll([chatMessages]);
+
+  return (
+    <div className="chat-message-container" ref={chatMessagesRef}>
+      {chatMessages.map((chatMessage) => {
+        return (
+          <ChatMessage
+            message={chatMessage.message}
+            sender={chatMessage.sender}
+            key={chatMessage.id}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+function App() {
+  // Here we did state lifting from chat component to App.
+  const [chatMessages, setChatMessages] = useState([])
+
+  // const [chatMessages, setChatMessages] = array
+  // const chatMessages = array[0];
+  // const setChatMessages = array[1];
+
+  return (
+    <div className="app-container">
+      {chatMessages.length === 0 && (
+        <p className="welcome-message">
+          Welcome to the chatbot project! Send a message using the textbox below.
+        </p>
+      )}
+      <ChatMessages
+        chatMessages={chatMessages}
+      />
+      <ChatInput
+        chatMessages={chatMessages}
+        setChatMessages={setChatMessages}
+      />
+    </div>
+  )
+}
+
+export default App
