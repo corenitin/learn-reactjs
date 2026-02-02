d94002fff1b9b0adf55fdad9391662770fd43946
nitin <152731976+corenitin@users.noreply.github.com>
2026-02-02 13:54:09 +0000

    changes in style

--- chatbot-project/src/components/ChatMessage.jsx
+++ chatbot-project/src/components/ChatMessage.jsx
@@ -1,5 +1,5 @@
 import RobotProfileImage from '../assets/robot.png'
-import UserProfileImage from '../assets/user.png'
+import UserProfileImage from '../assets/me.png'
 import './ChatMessage.css'
 
 export function ChatMessage({ message, sender }) {
@@ -19,7 +19,7 @@ export function ChatMessage({ message, sender }) {
       )
   }
   */
-
+console.log(UserProfileImage)
   return (
     <div className={
       sender === 'user'
