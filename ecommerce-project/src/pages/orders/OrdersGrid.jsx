076402386c925060f6c4dca6e9402434d6610004
nitin <152731976+corenitin@users.noreply.github.com>
2026-02-04 18:40:35 +0000

    create multiple order page component

---
+++ ecommerce-project/src/pages/orders/OrdersGrid.jsx
@@ -0,0 +1,19 @@
+import dayjs from "dayjs"
+import { OrderHeader } from "./OrderHeader"
+import { OrderDetailsGrid } from "./OrderDetailsGrid"
+
+export function OrdersGrid({orders}) {
+    return (
+        <div className="orders-grid">
+            {orders.map((order) => {
+                return (
+                    <div key={order.id} className="order-container">
+
+                       <OrderHeader order={order} />
+                       <OrderDetailsGrid order={order}/>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
