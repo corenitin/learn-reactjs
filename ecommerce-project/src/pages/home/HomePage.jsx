6198da297e2f700b1a89b06a12ee9911ea3f5fbd
nitin <152731976+corenitin@users.noreply.github.com>
2026-02-09 06:31:32 +0000

    create working search bar

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
+import { useSearchParams } from 'react-router'
 import { Header } from '../../components/Header'
 import './HomePage.css'
 import { ProductsGrid } from './ProductsGrid'
@@ -8,19 +9,22 @@ import { ProductsGrid } from './ProductsGrid'
 export function HomePage({ cart, loadCart }) {
 
     const [products, setProducts] = useState([])
+    const [searchParams] = useSearchParams();
+    const search = searchParams.get('search');
 
     useEffect(() => {
         const getHomeData = async () => {
-            const response = await axios.get('/api/products')
+            const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+            const response = await axios.get(urlPath);
             setProducts(response.data);
         }
         getHomeData()
-    }, [])
+    }, [search]);
 
 
     return (
         <>
-            <title>Ecommerce Project</title>
+            <title>E-commerce Project</title>
             <link rel="icon" type="image/svg+xml" href="image.png" />
 
             <Header cart={cart} />
